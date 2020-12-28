474a33993dc915ffa47c64a78a390cb6f9949909
moka <gaoyuanfell@sina.com>
2020-12-28 15:03:02 +0800

    2020-12-28 15:03:01 → moka:gaoyuanfell@sina.com | msg → safe

--- service/src/middleware.ts
+++ service/src/middleware.ts
@@ -29,29 +29,8 @@ export function makeField() {
         mimetype: mimetype,
       };
       appendField(req.body, fieldname, file);
-
       let outStream = fs.createWriteStream(finalPath);
       fileStream.pipe(outStream);
-
-      // let chunks: Uint8Array[] = [];
-      // let length = 0;
-      // fileStream.on("data", (chunk) => {
-      //   chunks.push(chunk);
-      //   length += chunk.length;
-      // });
-      fileStream.on("end", () => {
-        // let arr = new ArrayBuffer(length);
-        // let uint = new Uint8Array(arr);
-        // let len = 0;
-        // for (let i = 0; i < chunks.length; i++) {
-        //   const chunk = chunks[i];
-        //   for (let j = 0; j < chunk.length; j++) {
-        //     uint[len] = chunk[len];
-        //     len++;
-        //   }
-        // }
-        // appendField(req.body[fieldname], "buffers", chunks);
-      });
     });
     busboy.on("field", (fieldname, value) => {
       appendField(req.body, fieldname, value);
@@ -61,6 +40,86 @@ export function makeField() {
     });
     req.pipe(busboy);
   };
+}
+
+function chunkFinish(
+  pathCfgTmp: string,
+  chunkCount: number,
+  chunkIndex: number
+) {
+  // 读取上传详情文件查看分片上传状态
+  let buf = fs.readFileSync(pathCfgTmp);
+
+  let arr = pickTypedArrayBuffer(buf);
+  let byteOffset = 0;
+  let fi = [];
+  // let chunkCount = arr.byteLength / 12;
+  for (let index = 0; index < chunkCount; index++) {
+    byteOffset = index * 12;
+    let a = new Uint32Array(arr, byteOffset);
+    let b = new Uint8Array(arr, byteOffset + 4);
+    if (index === +chunkIndex) {
+      b[0] = 1;
+    }
+    fi.push({
+      index: a[0],
+      complete: b[0],
+    });
+  }
+  // 写入修改后的状态
+  fs.writeFileSync(pathCfgTmp, new Uint8Array(arr));
+  return fi;
+}
+
+function readCacheFile(
+  req: Request,
+  next: NextFunction,
+  pathTmp: string,
+  pathCfgTmp: string
+) {
+  let {
+    sizes,
+    size,
+    byteOffset,
+    position,
+    chunkIndex,
+    chunkCount,
+    md5,
+    type,
+    fieldname,
+  } = req.body;
+
+  let { originalname, encoding, mimetype, finalPath, buffers } = req.body[
+    fieldname
+  ];
+
+  let fd = fs.openSync(pathTmp, "r+");
+  // 读取缓存文件
+  let rs = fs.createReadStream(finalPath);
+  let _position = +position;
+  rs.on("data", (chunk) => {
+    let __position = _position;
+    if (chunk instanceof Uint8Array) {
+      _position += chunk.length;
+      fs.write(fd, chunk, 0, chunk.length, __position, () => {});
+    }
+  });
+  rs.on("end", async () => {
+    fs.closeSync(fd);
+
+    // 删除缓存文件
+    fs.unlinkSync(finalPath);
+
+    let fi = chunkFinish(pathCfgTmp, +chunkCount, +chunkIndex);
+    // 上传完成后将文件移动到正式文件夹下，并删除分片状态文件
+    if (fi.filter((f) => f.complete === 1).length === +chunkCount) {
+      fs.unlinkSync(pathCfgTmp);
+      let _path = `${getUploadsPath()}/${md5}.${nameSuffix(originalname)}`;
+      fs.renameSync(pathTmp, _path);
+      appendField(req.body[fieldname], "path", _path);
+    }
+    next();
+  });
 }
 
 /***
@@ -83,90 +142,33 @@ export function makeUpload() {
       fieldname,
     } = req.body;
 
-    const setp = async () => {
-      let { originalname, encoding, mimetype, finalPath, buffers } = req.body[
-        fieldname
-      ];
+    let { originalname, encoding, mimetype, finalPath, buffers } = req.body[
+      fieldname
+    ];
 
-      // 分片待写入文件
-      let pathTmp = `${getNowTempPath()}/${md5}.${nameSuffix(originalname)}`;
-      if (!fs.existsSync(pathTmp)) {
-        fs.writeFileSync(pathTmp, new Uint8Array());
+    // 分片待写入文件
+    let pathTmp = `${getNowTempPath()}/${md5}.${nameSuffix(originalname)}`;
+    if (!fs.existsSync(pathTmp)) {
+      fs.writeFileSync(pathTmp, new Uint8Array());
+    }
+
+    // 分片上传状态
+    let pathCfgTmp = `${getNowTempPath()}/${md5}.${nameSuffix(
+      originalname
+    )}.cfg`;
+    if (!fs.existsSync(pathCfgTmp)) {
+      let arr = new ArrayBuffer(+chunkCount * 12);
+      let byteOffset = 0;
+      for (let index = 0; index < +chunkCount; index++) {
+        byteOffset = index * 12;
+        let u = new Uint32Array(arr, byteOffset);
+        u[0] = index;
+        let b = new Uint8Array(arr, byteOffset + 4);
+        b[0] = 0;
       }
+      fs.writeFileSync(pathCfgTmp, new Uint8Array(arr));
+    }
 
-      // 分片上传状态
-      let pathCfgTmp = `${getNowTempPath()}/${md5}.${nameSuffix(
-        originalname
-      )}.cfg`;
-      if (!fs.existsSync(pathCfgTmp)) {
-        let arr = new ArrayBuffer(+chunkCount * 12);
-        let byteOffset = 0;
-        for (let index = 0; index < +chunkCount; index++) {
-          byteOffset = index * 12;
-          let u = new Uint32Array(arr, byteOffset);
-          u[0] = index;
-          let b = new Uint8Array(arr, byteOffset + 4);
-          b[0] = 0;
-        }
-        fs.writeFileSync(pathCfgTmp, new Uint8Array(arr));
-      }
-
-      let fd = fs.openSync(pathTmp, "r+");
-
-      // 读取缓存文件
-      let rs = fs.createReadStream(finalPath);
-      let _position = +position;
-      rs.on("data", (chunk) => {
-        let __position = _position;
-        if (chunk instanceof Uint8Array) {
-          _position += chunk.length;
-          fs.write(fd, chunk, 0, chunk.length, __position, () => {});
-        }
-      });
-
-      rs.on("end", async () => {
-        fs.closeSync(fd);
-
-        // 删除缓存文件
-        fs.unlinkSync(finalPath);
-
-        // 读取上传详情文件查看分片上传状态
-        let buf = fs.readFileSync(pathCfgTmp);
-
-        let arr = pickTypedArrayBuffer(buf);
-        let byteOffset = 0;
-        let fi = [];
-        for (let index = 0; index < +chunkCount; index++) {
-          byteOffset = index * 12;
-          let a = new Uint32Array(arr, byteOffset);
-          if (byteOffset + 4 > arr.byteLength) {
-            console.info(byteOffset + 4 > arr.byteLength);
-          }
-          let b = new Uint8Array(arr, byteOffset + 4);
-          if (index === +chunkIndex) {
-            b[0] = 1;
-          }
-          fi.push({
-            index: a[0],
-            complete: b[0],
-          });
-        }
-        // 写入修改后的状态
-        // await fs.promises.writeFile(pathCfgTmp, new Uint8Array(arr));
-        fs.writeFileSync(pathCfgTmp, new Uint8Array(arr));
-
-        // 上传完成后将文件移动到正式文件夹下，并删除分片状态文件
-        if (fi.filter((f) => f.complete === 1).length === +chunkCount) {
-          fs.unlinkSync(pathCfgTmp);
-          let _path = `${getUploadsPath()}/${md5}.${nameSuffix(originalname)}`;
-          fs.renameSync(pathTmp, _path);
-          appendField(req.body[fieldname], "path", _path);
-        }
-
-        next();
-      });
-    };
-
-    setp();
+    readCacheFile(req, next, pathTmp, pathCfgTmp);
   };
 }
